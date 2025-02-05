d758fb7509ed41e99ffcbfdf17a5208b422550b0
Josh Segar <joseg@equinor.com>
2025-02-05 19:23:09 +0100

    added good control and sound

--- main.ts
+++ main.ts
@@ -1,77 +1,125 @@
-function init () {
-    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmDirectionInit, 1000)
-    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmAngleInit, 1000)
+input.onButtonPressed(Button.A, function () {
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Red)
+    StartbitV2.startbit_showLight()
+    ArmDirectionInit = (ArmDirectionTopLimit - ArmDirectionBottomLimit) / 2 + ArmDirectionBottomLimit
+    ArmAngleInit = (ArmAngleTopLimit - ArmAngleBotomLimit) / 2 + ArmAngleBotomLimit
+    ClawInit = (ClawTopLimit - ClawBottomLimit) / 2 + ClawBottomLimit
+    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmAngleInit, 1000)
+    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmDirectionInit, 1000)
     StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawInit, 1000)
-    basic.showString("F")
-    StartbitV2.startbit_setMotorSpeed(80, 80)
-    basic.showString("B")
-    StartbitV2.startbit_setMotorSpeed(-80, -80)
-    basic.showString("L")
-    StartbitV2.startbit_setMotorSpeed(-80, 80)
-    basic.showString("R")
-    StartbitV2.startbit_setMotorSpeed(80, -80)
-    basic.showString("S")
-    StartbitV2.startbit_setMotorSpeed(0, 0)
-}
-radio.onReceivedValue(function (name, value) {
-    Speed = pins.map(
-    value,
-    -5,
-    5,
-    -100,
-    100
-    )
-    if (name == "34") {
-        basic.showString("F")
-        StartbitV2.startbit_setMotorSpeed(Speed, Speed)
-    } else if (name == "56") {
-        if (value < 0) {
-            basic.showString("R")
-            StartbitV2.startbit_setMotorSpeed(Speed, Speed / 2)
-        } else {
-            basic.showString("L")
-            StartbitV2.startbit_setMotorSpeed(Speed / 2, Speed)
-        }
-    } else if (name == "90") {
-        basic.showString("S")
-        ArmDirectionCurrent = pins.map(
-        value,
-        -5,
-        5,
-        ArmDirectionBottomLimit,
-        ArmDirectionTopLimit
-        )
-        StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmDirectionCurrent, 1000)
-    } else if (name == "ArmLift") {
-        basic.showString("E")
-        ArmAngleCurrent = pins.map(
-        value,
-        -5,
-        5,
-        ArmAngleBotomLimit,
-        ArmAngleTopLimit
-        )
-        StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmAngleCurrent, 1000)
-    } else {
-    	
-    }
-    if (name == "1") {
-        basic.showString("O")
-        ClawCurrent = ClawCurrent - 15
-        StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawCurrent, 1000)
-    } else if (name == "2") {
-        basic.showString("C")
-        ClawCurrent = ClawCurrent + 15
-        StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawCurrent, 1000)
-    } else {
-    	
-    }
+    basic.showIcon(IconNames.Yes)
+    StartbitV2.startbit_clearLight()
+    basic.clearScreen()
 })
-let Speed = 0
+radio.onReceivedString(function (receivedString) {
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Red)
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light2, StartbitRGBColors.Red)
+    StartbitV2.startbit_showLight()
+    if (receivedString == "stop") {
+        StartbitV2.startbit_setMotorSpeed(0, 0)
+    } else {
+        if (receivedString == "CO") {
+            if (ClawCurrent < ClawTopLimit) {
+                ClawCurrent += 10
+                StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawCurrent, 1000)
+            }
+        } else {
+            if (receivedString == "CC") {
+                if (ClawCurrent > ClawBottomLimit) {
+                    ClawCurrent += -10
+                    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawCurrent, 1000)
+                }
+            } else {
+                if (receivedString == "J2") {
+                    music.play(music.stringPlayable("E E - E E - A A ", 220), music.PlaybackMode.UntilDone)
+                }
+                if (receivedString == "J1") {
+                    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+                }
+            }
+        }
+    }
+    StartbitV2.startbit_clearLight()
+})
+radio.onReceivedValue(function (name, value) {
+    let RY = 0
+    let RX = 0
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Red)
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light2, StartbitRGBColors.Red)
+    StartbitV2.startbit_showLight()
+    if (name == "LX") {
+        if (value < 0) {
+            LX = value
+            StartbitV2.startbit_setMotorSpeed(value * -1, value)
+        } else {
+            LX = value
+            StartbitV2.startbit_setMotorSpeed(value, value * -1)
+        }
+    }
+    if (name == "LY") {
+        LY = value
+        StartbitV2.startbit_setMotorSpeed(value, value)
+    }
+    if (name == "RX") {
+        if (ArmDirectionCurrent > ArmDirectionBottomLimit) {
+            ArmDirectionCurrent += -10
+            StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmDirectionCurrent, 500)
+        } else {
+            if (ArmDirectionCurrent > ArmDirectionBottomLimit) {
+                ArmDirectionCurrent += -10
+                StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmDirectionCurrent, 500)
+            }
+        }
+    }
+    if (name == "RY") {
+        if (ArmAngleCurrent < ArmAngleTopLimit) {
+            ArmAngleCurrent += 20
+            StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmAngleCurrent, 500)
+        } else {
+            if (ArmAngleCurrent > ArmAngleBotomLimit) {
+                ArmAngleCurrent += -20
+                StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmAngleCurrent, 500)
+            }
+        }
+    }
+    led.plot(pins.map(
+    LX,
+    -100,
+    100,
+    0,
+    4
+    ), pins.map(
+    LY,
+    -100,
+    100,
+    0,
+    4
+    ))
+    led.plot(pins.map(
+    RX,
+    -100,
+    100,
+    0,
+    4
+    ), pins.map(
+    RY,
+    -100,
+    100,
+    0,
+    4
+    ))
+    basic.pause(50)
+    basic.clearScreen()
+    StartbitV2.startbit_clearLight()
+})
+let LY = 0
+let LX = 0
 let ArmAngleCurrent = 0
 let ArmDirectionCurrent = 0
 let ClawCurrent = 0
 let ClawInit = 0
+let ClawBottomLimit = 0
+let ClawTopLimit = 0
 let ArmAngleInit = 0
 let ArmDirectionInit = 0
 let ArmDirectionBottomLimit = 0
@@ -83,6 +131,8 @@ let LiftServo = 0
 let ArmServo = 0
 StartbitV2.startbit_Init()
 radio.setGroup(1)
+basic.showNumber(1)
+basic.showIcon(IconNames.Yes)
 ArmServo = 1
 LiftServo = 2
 ClawServo = 3
@@ -90,12 +140,20 @@ ArmAngleTopLimit = 110
 ArmAngleBotomLimit = 45
 ArmDirectionTopLimit = 130
 ArmDirectionBottomLimit = 25
-ArmDirectionInit = 78
-ArmAngleInit = 70
-let ClawTopLimit = 150
-let ClawBottomLimit = 90
-ClawInit = 120
+ArmDirectionInit = (ArmDirectionTopLimit - ArmDirectionBottomLimit) / 2 + ArmDirectionBottomLimit
+ArmAngleInit = (ArmAngleTopLimit - ArmAngleBotomLimit) / 2 + ArmAngleBotomLimit
+ClawTopLimit = 150
+ClawBottomLimit = 90
+ClawInit = (ClawTopLimit - ClawBottomLimit) / 2 + ClawBottomLimit
 ClawCurrent = ClawInit
 ArmDirectionCurrent = ArmDirectionInit
 ArmAngleCurrent = ArmAngleInit
-basic.showIcon(IconNames.SmallHeart)
+StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ArmServo, ArmAngleInit, 1000)
+StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, LiftServo, ArmDirectionInit, 1000)
+StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, ClawServo, ClawInit, 1000)
+StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Green)
+StartbitV2.startbit_setPixelRGB(StartbitLights.Light2, StartbitRGBColors.Green)
+basic.clearScreen()
+basic.forever(function () {
+    led.plot(2, 2)
+})
